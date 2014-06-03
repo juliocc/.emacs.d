79f3f28c49ad48df5f0b13bc3659d0048f80c080
Julio Castillo <juliocc@gmail.com>
2014-06-03 12:49:24 +0200

    Removing which-function-mode from the header

--- init.el
+++ init.el
@@ -380,12 +380,14 @@
 ;; which-func-mode settings
 ;;==================================================
 (which-function-mode 1)                 ; show me where I'm standing
-(setq-default header-line-format
-              '((which-func-mode ("" which-func-format " "))))
-(setq mode-line-misc-info
-            ;; We remove Which Function Mode from the mode line, because it's mostly
-            ;; invisible here anyway.
-            (assq-delete-all 'which-func-mode mode-line-misc-info))
+
+; puts which-function-mode in the header
+;; (setq-default header-line-format
+;;               '((which-func-mode ("" which-func-format " "))))
+;; (setq mode-line-misc-info
+;;             ;; We remove Which Function Mode from the mode line, because it's mostly
+;;             ;; invisible here anyway.
+;;             (assq-delete-all 'which-func-mode mode-line-misc-info))
 
 ;;==================================================
 ;; ispell settings
