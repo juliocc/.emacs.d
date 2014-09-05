4fa6fbff5a9bb5493291bc204f6683af882e0cd1
Julio Castillo <juliocc@gmail.com>
2014-09-05 16:04:00 +0200

    Add prelude prefix to guide-key

--- init.el
+++ init.el
@@ -371,7 +371,7 @@
 ;; guide-key setup
 (setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x v" "C-x 8"
                                      "C-x C-k" "<f8>" "C-c !" "M-s"
-                                     "C-x n"))
+                                     "C-x n" "C-c p"))
 (guide-key-mode 1)
 (setq guide-key/recursive-key-sequence-flag t)
 (setq guide-key/popup-window-position 'bottom)
