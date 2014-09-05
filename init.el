9b0485eea3ec4ef1e4ab7843963abf86f7240c7b
Julio Castillo <juliocc@gmail.com>
2014-09-05 16:03:53 +0200

    Test god-mode

--- init.el
+++ init.el
@@ -121,7 +121,8 @@
      clojure-mode
      cider
      fancy-narrow
-     discover-my-major)))
+     discover-my-major
+     god-mode)))
 
 (condition-case nil
     (init--install-packages)
@@ -838,6 +839,26 @@ is considered to be a project root."
 (setq web-mode-engines-alist
       '(("php" . "\\.phtml\\'")
         ("django" . "\\.html\\'")))
+
+;;==================================================
+;; god-mode settings
+;;==================================================
+(require 'god-mode)
+(global-set-key (kbd "<escape>") 'god-local-mode)
+
+(defun my-update-cursor ()
+  (setq cursor-type (if (or god-local-mode buffer-read-only)
+                        'box
+                      'bar)))
+
+(add-hook 'god-mode-enabled-hook 'my-update-cursor)
+(add-hook 'god-mode-disabled-hook 'my-update-cursor)
+(global-set-key (kbd "C-x C-1") 'delete-other-windows)
+(global-set-key (kbd "C-x C-2") 'split-window-below)
+(global-set-key (kbd "C-x C-3") 'split-window-right)
+(global-set-key (kbd "C-x C-0") 'delete-window)
+(define-key god-local-mode-map (kbd "z") 'repeat)
+(define-key god-local-mode-map (kbd ".") 'god-local-mode)
 
 ;;==================================================
 ;; Dired settings
