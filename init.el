dceb4d6729eb9d2c1668388f7a0f776f32551b0c
Julio Castillo <juliocc@gmail.com>
2014-11-02 17:37:49 +0100

    Updating melpa url

--- init.el
+++ init.el
@@ -38,7 +38,7 @@
 
 ;; Add melpa to package repos
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 ;; Add marmalade to repos
 ;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
