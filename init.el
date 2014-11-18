9cfcfc620102f963e55742ed0a959a4901c1f8b4
Julio Castillo <juliocc@gmail.com>
2014-11-18 20:27:47 +0000

    Moving my search stuff to its own file

--- init.el
+++ init.el
@@ -426,47 +426,17 @@
 
 (define-key isearch-mode-map (kbd "C-o") 'isearch-occur)
 
-;; TODO: Move to autoload
-;; http://www.emacswiki.org/emacs/ZapToISearch
-(defun zap-to-isearch (rbeg rend)
-  "Kill the region between the mark and the closest portion of
-the isearch match string. The behaviour is meant to be analogous
-to zap-to-char; let's call it zap-to-isearch. The deleted region
-does not include the isearch word. This is meant to be bound only
-in isearch mode. The point of this function is that oftentimes
-you want to delete some portion of text, one end of which happens
-to be an active isearch word. The observation to make is that if
-you use isearch a lot to move the cursor around (as you should,
-it is much more efficient than using the arrows), it happens a
-lot that you could just delete the active region between the mark
-and the point, not include the isearch word."
-  (interactive "r")
-  (when (not mark-active)
-    (error "Mark is not active"))
-  (let* ((isearch-bounds (list isearch-other-end (point)))
-         (ismin (apply 'min isearch-bounds))
-         (ismax (apply 'max isearch-bounds)))
-    (if (< (mark) ismin)
-        (kill-region (mark) ismin)
-      (if (> (mark) ismax)
-          (kill-region ismax (mark))
-        (error "Internal error in isearch kill function.")))
-    (isearch-exit)
-    ))
+(req-package jc-search
+  :commands (zap-to-isearch isearch-exit-other-end)
+  :init (bind-keys :map isearch-mode-map
+                   ("M-z" . zap-to-isearch)
+                   ("C-RET" . isearch-exit-other-end)))
 
 (define-key isearch-mode-map [(meta z)] 'zap-to-isearch)
-
-;; TODO: Move to autoload
-(defun isearch-exit-other-end (rbeg rend)
-  "Exit isearch, but at the other end of the search string.
-This is useful when followed by an immediate kill."
-  (interactive "r")
-  (isearch-exit)
-  (goto-char isearch-other-end))
-
 (define-key isearch-mode-map [(control return)] 'isearch-exit-other-end)
 
 (req-package anzu
+  :diminish anzu-mode
   :config
   (progn
     ;; show number of matches while searching
