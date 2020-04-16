dc7c17e0ea30d1055a3ceb154df62815de694eea
Julio Castillo <jccb@google.com>
2020-04-16 10:56:11 +0200

    more stuff from doom

--- init.el
+++ init.el
@@ -1550,4 +1550,30 @@ all hooks after it are ignored.")
   (solaire-global-mode +1)
   (solaire-mode-swap-bg))
 
-; doom: recentf better-jumber dtrt-indent smartparens so-long ws-butler pcre2el highlight-indent-guides
+(use-package hl-todo
+  :hook (prog-mode . hl-todo-mode)
+  :config
+  (setq hl-todo-highlight-punctuation ":"
+        hl-todo-keyword-faces
+        `(;; For things that need to be done, just not today.
+          ("TODO" warning bold)
+          ;; For problems that will become bigger problems later if not
+          ;; fixed ASAP.
+          ("FIXME" error bold)
+          ;; For tidbits that are unconventional and not intended uses of the
+          ;; constituent parts, and may break in a future update.
+          ("HACK" font-lock-constant-face bold)
+          ;; For things that were done hastily and/or hasn't been thoroughly
+          ;; tested. It may not even be necessary!
+          ("REVIEW" font-lock-keyword-face bold)
+          ;; For especially important gotchas with a given implementation,
+          ;; directed at another user other than the author.
+          ("NOTE" success bold)
+          ;; For things that just gotta go and will soon be gone.
+          ("DEPRECATED" font-lock-doc-face bold))))
+
+;; doom: recentf better-jumber dtrt-indent smartparens so-long
+;; ws-butler pcre2el highlight-indent-guides doom/escape auto-revert
+;; company ivy nav-flash workspaces
+;;
+;; use-package defer
