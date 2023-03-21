a9cf8f32b387f8d4746090b60c916b364e2309e1
Julio Castillo <jccb@google.com>
2023-03-21 15:37:41 +0100

    fix fabric functions

--- site-lisp/jccb-fabric.el
+++ site-lisp/jccb-fabric.el
@@ -1,3 +1,19 @@
+(defvar jccb/fabric-boilerplate "# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the \"License\");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an \"AS IS\" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+")
+
 (defun jccb/fabric-create-open-example-inventory ()
   (interactive)
   (beginning-of-line)
@@ -20,7 +36,7 @@
         (write-region (save-excursion (+ (search-backward "```hcl") 6))
                       (save-excursion (- (search-forward "```") 3))
                       "~/tmp/code.tf"))
-      (insert (get-register ?c))
+      (insert jccb/fabric-boilerplate)
       (insert (shell-command-to-string
                (format "python %s --example ~/tmp/code.tf"
                        (f-join fabric-path "tools/plan_summary.py")))))))
